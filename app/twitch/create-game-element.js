1146eafa4ccbaee86ac8bfacb88f3c9934027f5a
Zach Edwards <tolklorist@gmail.com>
2017-02-17 16:17:58 -0600

    changes

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,1 +1,36 @@
-export default null;
+export default function createGameItem(game) {
+  const gameItem = document.createElement('div');
+
+  gameItem.classList.add('game-item');
+
+  const gameItemName = document.createElement('h3');
+
+  gameItemName.classList.add('game-item__name');
+
+  gameItem.appendChild(gameItemName);
+
+  gameItemName.innerText = game.name;
+
+  const gameItemPopularity = document.createElement('h4');
+
+  gameItemPopularity.classList.add('game-item__popularity');
+
+  gameItem.appendChild(gameItemPopularity);
+
+  gameItemPopularity.innerText = game.popularity + ' Viewers';
+
+
+  const gameItemPic = document.createElement('img');
+
+  gameItemPic.classList.add('game-item__pic');
+
+  gameItem.appendChild(gameItemPic);
+
+
+
+
+
+  return gameItem;
+
+
+}
