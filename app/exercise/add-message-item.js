30c6138075d118449b455879222ffb4bf8938feb
Zach Edwards <tolklorist@gmail.com>
2017-02-17 11:01:09 -0600

    changes

--- app/exercise/add-message-item.js
+++ app/exercise/add-message-item.js
@@ -1,6 +1,6 @@
 import createMessageItem from './create-message-item';
 
-export default addMessageItem(list, message) {
-  const listItem = createMessageItem(message);
-  list.appendChild(listItem);
-}
+//export default addMessageItem(list, message) {
+//  const listItem = createMessageItem(message);
+//  list.appendChild(listItem);
+//}
