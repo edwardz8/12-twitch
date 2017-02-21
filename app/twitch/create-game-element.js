162cc3ca40423ba61460d2015917f9390d5832d1
Zach Edwards <tolklorist@gmail.com>
2017-02-21 15:48:23 -0600

    finished

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -2,9 +2,9 @@ export default function createGameElement(game) {
  const el = document.createElement('div');
  el.classList.add('game-item');
  el.innerHTML = `
- <h3 class="title game-item__name"></h3>
- <h4 class="views game-item__popularity"></h4>
- <img class="photo game-item__pic">`;
+  <img class="photo game-item__pic">
+  <h3 class="title game-item__name"></h3>
+  <h4 class="views game-item__popularity"></h4>`;
  el.querySelector('.game-item__name').innerText = `${game.name}`;
  el.querySelector('.game-item__popularity').innerText = `${game.popularity} Viewers`;
  el.querySelector('.game-item__pic').src = game.box.large;
